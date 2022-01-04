6c223e48ea0c34fb22233f7c86a0ca1caeb1cb54
manan <manan@pedagogy.study>
2022-01-04 23:31:22 +0530

    Adding node at the front end of the linked list

---
+++ LinkedList/src/_2_insertion/Insertion.java
@@ -0,0 +1,19 @@
+package _2_insertion;
+
+import base.MyLinkedList;
+
+public class Insertion {
+
+	public static void main(String[] args) {
+		var linkedList = new MyLinkedList();
+		linkedList.append(5);
+		linkedList.append(15);
+		linkedList.append(25);
+		linkedList.printList();
+		linkedList.push(3);
+		linkedList.printList();
+		linkedList.push(1);
+		linkedList.printList();
+	}
+
+}
